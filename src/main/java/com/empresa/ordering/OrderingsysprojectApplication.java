d236fc72d8cf621fe254b6364f345090d07912e8
luanrnunes <luanrobertonunes@hotmail.com>
2022-01-23 18:05:32 -0300

    Iniciando repositório

---
+++ src/main/java/com/empresa/ordering/OrderingsysprojectApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingsysprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingsysprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/ordering/OrderingsysprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingsysprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
