8ec19dec1189e43b7b973c4a0ffd7739f2bd5b60
luanrnunes <luanrobertonunes@hotmail.com>
2022-01-29 22:53:29 -0300

    Delecao de usuarios

--- src/main/java/com/empresa/ordering/resources/UserResource.java
+++ src/main/java/com/empresa/ordering/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,5 +59,13 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
+	
+	/*DeleteMapping notation do spring que indica que o recurso pode receber uma requisicao de delete*/
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) { /*PathVariable indica que o argumento passado na requisicao HTTP sera aceito*/
+	service.delete(id);
+	return ResponseEntity.noContent().build(); /*Indica que a resposta desta requisicao sera vazia, retornando um 204*/
+}
 	
 }
