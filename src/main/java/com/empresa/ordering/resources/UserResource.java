166103a699fd7d10044512099ef00a5fbae0d753
luanrnunes <luanrobertonunes@hotmail.com>
2022-01-29 22:42:01 -0300

    Insercao de usuarios

--- src/main/java/com/empresa/ordering/resources/UserResource.java
+++ src/main/java/com/empresa/ordering/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.empresa.ordering.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.empresa.ordering.entities.User;
 import com.empresa.ordering.services.UserService;
@@ -17,6 +21,7 @@ import com.empresa.ordering.services.UserService;
  * RequestMapping da o caminho onde a REST ira responder 
  * (ex, digitar no navegador localhost:8080/user ira retornar o que foi definido na classe)*/
 
+
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
@@ -24,7 +29,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	/*Para indicar que este metodo responde a requisicoes GET do HTTP, é utilizado GetMapping */
+	/*Para indicar que o metodo é a resposta GET do HTTP, é utilizado GetMapping */
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
@@ -43,5 +48,15 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	/*PostMapping. Notation do spring que faz um pre processamento na compilacao e indica que vai responder
+	a uma requisicao post do HTTP*/
+	
+	@PostMapping 
+	public ResponseEntity<User> insert (@RequestBody User obj) { /*RequestBody indica que a resposta sera um JSON que precisa ser desserializado, neste caso, desserializado para um objeto User*/
+		obj = service.insert(obj);
+		/*Para retornar com o codigo 201 e um header ao inves do padrao 200*/
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 	
 }
