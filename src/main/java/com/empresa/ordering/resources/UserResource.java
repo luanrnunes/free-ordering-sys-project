473b945134fc312d205a0554c0e31f8c0a3e9027
luanrnunes <luanrobertonunes@hotmail.com>
2022-01-29 23:10:08 -0300

    Atualizacao de User

--- src/main/java/com/empresa/ordering/resources/UserResource.java
+++ src/main/java/com/empresa/ordering/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,5 +68,12 @@ public class UserResource {
 	service.delete(id);
 	return ResponseEntity.noContent().build(); /*Indica que a resposta desta requisicao sera vazia, retornando um 204*/
 }
+	
+	/*PutMapping notation que indica que pode receber requisicao PUT do HTTP*/
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
 	
 }
--- src/main/java/com/empresa/ordering/services/UserService.java
+++ src/main/java/com/empresa/ordering/services/UserService.java
@@ -35,5 +35,18 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id); /*getOne prepara um objeto para operacao posterior*/
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 
 }
